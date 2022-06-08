8c33d52d7eb5213c60138cb040bc5945c4732b6b
jiwon111 <dreamjiwon@naver.com>
2022-06-08 20:04:27 +0900

    solve

--- 2667.py
+++ 2667.py
@@ -2,35 +2,32 @@ n = int(input())
 cnt = 0
 graph = []
 house = []
-global house_cnt
 for _ in range(n):
     graph.append(list(map(int, input())))
-print(graph)
 
 house_cnt=0
-def dfs(i, j, house, house_cnt):
+def dfs(i, j, house):
+    global house_cnt
     if i<0 or j<0 or i>=n or j>=n:
         return False
     if graph[i][j]==1:
-        graph[i][j] = 0
-
-        dfs(i, j+1, house, house_cnt)
-        dfs(i, j-1, house, house_cnt)
-        dfs(i-1, j, house, house_cnt)
-        dfs(i+1, j, house, house_cnt)
         house_cnt+=1
-        print('house_cnt=', house_cnt)
+        graph[i][j] = 0
+        dfs(i, j+1, house)
+        dfs(i, j-1, house)
+        dfs(i-1, j, house)
+        dfs(i+1, j, house)
         return True
     return False
 
 for i in range(n):
     for j in range(n):
-        if dfs(i, j, house, house_cnt):
+        if dfs(i, j, house):
             cnt+=1
-            print('여기', house_cnt)
             house.append(house_cnt)
             house_cnt=0
 
-
 print(cnt)
-print(house)
+house.sort()
+for i in house:
+    print(i)
